552f96c849a7bd67bfcedcedee09505a7e77cc8a
Albin Reji <albinrejim30@gmail.com>
2026-02-21 19:37:08 +0530

    define model User

--- user-service/src/main/java/com/nearca/user_service/model/UserRole.java
+++ user-service/src/main/java/com/nearca/user_service/model/UserRole.java
@@ -1,5 +1,13 @@
 package com.nearca.user_service.model;
 
+/**
+*{@link UserRole}
+ * <p>User Role types: </p>
+* <ul>
+ *     <li>User</li>
+ *     <li>ADMIN</li>
+* </ul>
+* */
 public enum UserRole {
     USER,
     ADMIN
