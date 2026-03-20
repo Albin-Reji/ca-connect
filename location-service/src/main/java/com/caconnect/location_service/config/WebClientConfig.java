95336c904a43033d103921ba5da61cdf96515558
Albin Reji <albinrejim30@gmail.com>
2026-03-20 19:09:28 +0530

    stage: devolop

--- location-service/src/main/java/com/caconnect/location_service/config/WebClientConfig.java
+++ location-service/src/main/java/com/caconnect/location_service/config/WebClientConfig.java
@@ -10,7 +10,7 @@ public class WebClientConfig {
     @Bean(name = "userServiceWebClient")
     public WebClient userServiceWebClient(){
         return WebClient.builder()
-                .baseUrl("http://gateway:8080")
+                .baseUrl("http://localhost:8080")
                 .build();
     }
 }
